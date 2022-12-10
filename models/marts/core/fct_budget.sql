88dc24d8da906d5f7a6395f8f01189dd37d3e23c
94victorgm@gmail.com <94victorgm@gmail.com>
2022-12-10 10:36:35 +0000

    10 diciembre am

--- models/marts/core/fct_budget.sql
+++ models/marts/core/fct_budget.sql
@@ -1,19 +1,28 @@
 with
     budget as (select * from {{ ref('stg_google_sheets_budget') }}),
 
+  --  dim_fecha as (select * from {{ref('dim_fecha')}}),
+
+   -- producto_budget  as (select * from {{ ref('stg_sql_server_dbo_products') }}),
+
 
 
     presupuestos as (
         select
           fila
-        , product_id 
+       -- , producto_budget.product_id 
         , cantidad
         , fecha
-
+       -- , dim_fecha.id_date
+     --   , mes
+      --  , inventario
+    --    , precio
+        
 
         from budget
-  
-
+    --    join dim_fecha on dim_fecha.fecha_forecast = cast(budget.fecha as date)
+     --   join producto_budget on producto_budget.product_id = budget.product_id
+      --  where mes = 3 
     )
 
 select *
--- models/marts/core/fct_products_orders_items.sql
+++ models/marts/core/fct_products_orders_items.sql
@@ -11,13 +11,12 @@ with
         , lineas_pedido.order_id
         , precio
         , cantidad
-        , precio*cantidad
+        , precio*cantidad as precio_final
         , pedido.user_id
         , id_nombre_planta
         from articulos
         join lineas_pedido on articulos.product_id = lineas_pedido.product_id
         join pedido on pedido.order_id = lineas_pedido.order_id
-
     )
 
 select *
